3c489fcd1470daa8afd35937031940a754364f7f
patryk1303 <patryk1303@gmail.com>
2015-04-23 16:40:29 +0200

    Dots display - initial change

--- MatrixDisplay/MatrixDisplay.cs
+++ MatrixDisplay/MatrixDisplay.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,59 @@ namespace MatrixDisplay
 {
     public class MatrixDisplay : UserControl
     {
+        private Color OnColor;
+        [Category("Colors")]
+        public Color DotEnabledColor
+        {
+            get { return OnColor; }
+            set { OnColor = value; }
+        }
+        private Color OffColor;
+        [Category("Colors")]
+        public Color DotDisabledColor
+        {
+            get { return OffColor; }
+            set { OffColor = value; }
+        }
+
+        private int DotSize;
+        [Category("Appearance")]
+        public int DotRadius
+        {
+            get { return DotSize; }
+            set { DotSize = value; }
+        }
+
         public MatrixDisplay() 
         {
+            ForeColor = Color.Black;
+            DotDisabledColor = Color.Gray;
+            DotEnabledColor = Color.Orange;
+            Width = 300;
+            Height = 48;
+            DotRadius = 16;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            int l = ClientRectangle.Left;
+            int t = ClientRectangle.Top;
+            int h = ClientRectangle.Height;
+            int w = ClientRectangle.Width;
+
+            int dotsOnWidth = Width / (DotRadius * 2);
+            int dotsOnHeight = Height / (DotRadius * 2);
+
+            Pen pen = new System.Drawing.Pen(DotDisabledColor,2);
+            SolidBrush brush = new SolidBrush(DotDisabledColor);
             
+            for(int i=0;i<dotsOnWidth;++i)
+            {
+                for(int j=0;j<dotsOnHeight;++j)
+                {
+                    e.Graphics.FillEllipse(brush, i * DotRadius + DotRadius * 2, j * DotRadius + DotRadius * 2, DotRadius * 2, DotRadius * 2);
+                }
+            }
         }
     }
 }
---
+++ MatrixTestWidnowApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MatrixTestWidnowApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.matrixDisplay1 = new MatrixDisplay.MatrixDisplay();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // matrixDisplay1
+            // 
+            this.matrixDisplay1.DotDisabledColor = System.Drawing.Color.Gray;
+            this.matrixDisplay1.DotEnabledColor = System.Drawing.Color.Orange;
+            this.matrixDisplay1.DotRadius = 23;
+            this.matrixDisplay1.ForeColor = System.Drawing.Color.Black;
+            this.matrixDisplay1.Location = new System.Drawing.Point(12, 38);
+            this.matrixDisplay1.Name = "matrixDisplay1";
+            this.matrixDisplay1.Size = new System.Drawing.Size(300, 193);
+            this.matrixDisplay1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 261);
+            this.Controls.Add(this.matrixDisplay1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private MatrixDisplay.MatrixDisplay matrixDisplay1;
+    }
+}
+
