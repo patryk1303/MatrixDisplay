f2c159e29a2d85da11c2b25b6bf758a5cd03ea3d
patryk1303 <patryk1303@gmail.com>
2015-04-23 16:17:14 +0200

    Initial preparation

---
+++ MatrixDisplay/MatrixDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixDisplay
+{
+    public class MatrixDisplay : UserControl
+    {
+        public MatrixDisplay() 
+        {
+            
+        }
+    }
+}
